e1a55f2536ee93a5e857440d4eeb2c505f7249b6
Nate Mortensen <nmortensen@Nathans-MacBook-Air.local>
2013-12-07 10:11:38 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PoemAnnotator::Application.config.secret_key_base = 'c3a88fad6391576a3aac2cd209f31a8058439e924a426e72d441ad0a73eb508c21f86e6b7858cb3f67c17a9a42be485cd234fd63e926614d97f81e6f96596323'
